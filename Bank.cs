c27632c6eb8972fde6b5dc93c9fa531adce4853f
Sheetal Samnani <sheetal_jn@hotmail.com>
2020-02-27 22:51:27 +0000

    Add project files.

---
+++ Bank.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    public class Bank
+    {
+        protected int AccBal;
+        protected int Withdraw;
+        protected int Dep;
+
+        public int WithdrawBal(int AccountBalance, int Withdrawal)
+        {
+            AccBal = AccountBalance;
+            Withdraw = Withdrawal;
+            AccBal = AccountBalance - Withdraw;
+            Console.WriteLine("Account balance after withdrawal is " + AccBal);
+            return AccBal;
+
+        }
+
+    }
+}
---
+++ BankChild.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class BankChild : Bank
+    {
+        int newAccbal;
+        public int Deposit(int Deposit)
+        {
+            Dep = Deposit;
+            int newAccbal = AccBal + Dep;
+            Console.WriteLine("New account balance after deposit is " + newAccbal);
+            return newAccbal;
+
+        }
+
+        static void Main(string[] args)
+        {
+            BankChild child = new BankChild();
+            child.WithdrawBal(1000, 200);
+            child.Deposit(500);
+        }
+
+
+    }
+}
